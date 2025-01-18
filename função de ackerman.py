c08a75c38e941200d02173d5aee42dfedfcfba85
Isac Rodrigues <isacrodrigues2011@live.com>
2025-01-17 23:08:16 -0300

    Ackermann function

---
+++ função de ackerman.py
@@ -0,0 +1,15 @@
+def ackermann(m, n):
+    """Computes the Ackermann function A(m, n)
+
+    See http://en.wikipedia.org/wiki/Ackermann_function
+
+    n, m: non-negative integers
+    """
+    print(m,n)
+    if m == 0:
+        return n+1
+    if n == 0:
+        return ackermann(m-1, 1)
+    return ackermann(m-1, ackermann(m, n-1))
+
+print(ackermann(5,5))
